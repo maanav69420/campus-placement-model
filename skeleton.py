c1269f744cfd05503c546771a6b4af317f17128e
maanav69420 <124855913+maanav69420@users.noreply.github.com>
2024-05-08 13:30:35 +0530

    updated skeleton file

--- skeleton.py
+++ skeleton.py
@@ -5,7 +5,9 @@ st.write("***Model type:*** Classification ")
 st.write("***Library Used:*** Pandas , Numpy , Matplotlib , Seaborn , Sklearn , Steamlit")
 st.write("## Dataset Used to train:")
 data = pd.read_csv("sample_dataset.csv")
-
+data = data.drop(['Unnamed: 0'] , axis = 1)
 st.write(data.sample(10))
 st.write("## Student Count: ")
-st.bar_chart(data['degree_type'])
+
+
+st.bar_chart(data = data , x = 'secondary_branch', y  ='secondary_percent')
