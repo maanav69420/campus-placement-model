e877ba81fe6bb06aee3ee5c67f1b2ec3f2a252a3
maanav69420 <124855913+maanav69420@users.noreply.github.com>
2024-05-17 08:12:10 +0530

    interface working , problem in execution

--- project/pages/working.py
+++ project/pages/working.py
@@ -1,8 +1,7 @@
 import pickle
 
-with open ("model.pkl" , 'rb') as f:
-    classifier = pickle.load(f)
-
+pickle_in = open("C:/Users/maanav aditya/OneDrive/Documents/GitHub/campus-placement-model/model.pkl", "rb")
+classifier = pickle.load(pickle_in)
 def predict_outcome(secondary_percent, secondary_branch, highschool_percent,
        high_school_branch, hsc_s, degree_percent, degree_type,
        work_xp, employment_test_percent, specialisation, mba_percent):
@@ -19,12 +18,12 @@ import streamlit as st
 st.write('Testing....')
 
 
-secondary_branch_val = ['Others' 'Central']
-high_school_branch_val = ['Others' 'Central']
-hsc_s_val = ['Commerce' 'Science' 'Arts']
-degree_type_val = ['Sci&Tech' 'Comm&Mgmt' 'Others']
-work_xp_val = ['No' 'Yes']
-specialisation_val = ['Mkt&HR' 'Mkt&Fin']
+secondary_branch_val = ['Others' ,'Central']
+high_school_branch_val = ['Others', 'Central']
+hsc_s_val = ['Commerce', 'Science' ,'Arts']
+degree_type_val = ['Sci&Tech' ,'Comm&Mgmt' ,'Others']
+work_xp_val = ['No' , 'Yes']
+specialisation_val = ['Mkt&HR' , 'Mkt&Fin']
 
 
 def main():
@@ -36,14 +35,34 @@ def main():
 
     st.markdown(html_temp , unsafe_allow_html = True)
     secondary_percent = st.text_input("mention the percentage u received in secondary school:") 
-    secondary_branch = st.radio("pick the branch you chose in secondary school:" , secondary_branch_val)
+
+    secondary_branch = st.radio("pick the branch you chose in secondary school:" , options = secondary_branch_val)
+
     highschool_percent =st.text_input("percentage scored in highschool:")
-    high_school_branch = st.radio("mention the branch you chose in highschool :" , high_school_branch_val)
+
+    high_school_branch = st.radio("mention the branch you chose in highschool :" , options =high_school_branch_val)
+
     hsc_s = st.radio("pick the subject you chose in high school:" , hsc_s_val)
-    degree_percent = st.radio("mention the percentage you recieved in college :")
-    degree_type = st.radio("pick the subject for degree:" , degree_type_val)
-    work_xp = st.radio("Do you have any work experience:" , work_xp_val)
+
+    degree_percent = st.text_input("mention the percentage you recieved in college :")
+
+    degree_type = st.radio("pick the subject for degree:" , options = degree_type_val)
+
+    work_xp = st.radio("Do you have any work experience:" , options =work_xp_val)
+
     employment_test_percent = st.text_input("mention your percentage in employment test")
-    specialisation = st.radio("mention your specialization:" , specialisation_val)
+
+    specialisation = st.radio("mention your specialization:" , options =specialisation_val)
+
     mba_percent = st.text_input("mention your mba percentage :")
     
+    result = ''
+    if st.button('predict'):
+        result = predict_outcome(secondary_percent, secondary_branch, highschool_percent,
+       high_school_branch, hsc_s, degree_percent, degree_type,
+       work_xp, employment_test_percent, specialisation, mba_percent
+       )
+    st.success('the outcome is {}'.format(result))
+
+if __name__ == '__main__':
+    main()
